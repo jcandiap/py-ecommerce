3e54da6615566622f2f1e0fb69e903d1fe50d75a
Jose Candia <josecandiap@gmail.com>
2023-05-28 14:48:21 -0400

    se agrega metodo para logear usuario

--- project/apps/user/views.py
+++ project/apps/user/views.py
@@ -1,5 +1,6 @@
 from django.http import HttpResponse, JsonResponse
 from django.shortcuts import render
+from .models import User
 
 app_name = 'user'
 # Create your views here.
@@ -21,11 +22,31 @@ def register(request):
 def validate_login(request):
     try:
         if request.method == "POST":
-            email = request.POST.get('username')
-            password = request.POST.get('password')
-            message = f"Hola, { email }"
-            return JsonResponse({ 'message': message })
+            return __check_login_info(request)
         return JsonResponse({ 'message': 'error al verificar' })
     except Exception as e:
         print(e)
         return JsonResponse({ 'error': e })
+    
+def __check_login_info(request) -> JsonResponse:
+    email = request.POST.get('email')
+    password = request.POST.get('password')
+    if email is None:
+        return JsonResponse({ 'error': 'email es requerido' })
+    if password is None:
+        return JsonResponse({ 'error': 'password es requerido' })
+    try:
+        user = User.objects.get(email = email)
+        if user is None:
+            return JsonResponse({ 'error': 'usuario no encontrado' })
+        if __check_password(password):
+            message = f'Inicio exitoso, bienvenido(a) { user.name } { user.last_name }'
+            return JsonResponse({ 'message': message })
+    except Exception:
+        return JsonResponse({ 'error': 'Usuario no se encuentra registrado' })
+    return JsonResponse({ 'error': 'Datos no coinciden para verificar al usuario' })
+
+def __check_password(user: User, password: str):
+    if user.password == password:
+        return True
+    return False
